ed09d7bfbf039f6ad935a3c294443d55f0916ae3
OlyaSkr <OlyaSkr@users.noreply.github.com>
2023-08-31 13:43:01 +0000

    Automated deployment: Thu Aug 31 13:43:01 UTC 2023 7370469eec8c5c12c5a90255947f615ba5315058

--- utils/tools.py
+++
@@ -1,9 +0,0 @@
-import allure
-
-
-def take_screenshot(page, name) -> None:
-    allure.attach(
-        body=page.screenshot(full_page=True),
-        name=name,
-        attachment_type=allure.attachment_type.PNG
-    )
